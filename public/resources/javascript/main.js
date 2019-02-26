39a7f46e5c6bcafba7bb9374e9884324bc92a1ca
M. Hogeveen <mhogeveen94@gmail.com>
2019-02-26 22:21:53 +0100

    Add portfolio section content

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+  // Portfolio Section Functionality
+  $('.row--button1, .row--button2, .row--button3').click(function() {
+      //$('i').toggleClass('fa-angle-down fa-angle-up');
+      if ($(this).hasClass('row--button1')) {
+        $('.row--description1').slideToggle();
+        $('.row--button1 i').toggleClass('fa-angle-down fa-angle-up');
+      } else if ($(this).hasClass('row--button2')) {
+        $('.row--description2').slideToggle();
+        $('.row--button2 i').toggleClass('fa-angle-down fa-angle-up');
+      } else if ($(this).hasClass('row--button3')) {
+        $('.row--description3').slideToggle();
+        $('.row--button3 i').toggleClass('fa-angle-down fa-angle-up');
+      }
+    });
+
 });
